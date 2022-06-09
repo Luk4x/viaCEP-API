3d47d04bef37afda8af5832bec80143ab969594e
Luk4x <luk4xm4ci3l@gmail.com>
2022-06-09 19:11:53 -0300

    added CEP mask and dinamic fontsize

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
+// input focus keys (; and /)
 $(document.documentElement).keypress(keyP => {
     if (keyP.key == ';' || keyP.key === '/') {
         $('#cep-in').focus();
     }
 });
 
+// focus in/out animation
 $('#cep-in').focus(() => {
     $('.bx-search').toggleClass('bx-search-active');
 });
-
 $('#cep-in').focusout(() => {
     $('.bx-search').toggleClass('bx-search-active');
 });
 
+// hindering spinner on load
 $('.spinner-border').hide();
 
-$('#cep-in').on('input', () => {
+// masking input
+$('#cep-in').mask('00000-000');
+
+$('#cep-in').on('input change', () => {
+    // general things to reset on user input
     $('.viaCEP-api-data').empty();
     $('.map').empty();
     $('.catch-data').removeClass('catch-data-active');
     $('.user-input').removeClass('user-input-active');
-    $('.user-input').removeClass('invalid');
     $('.user-input').removeClass('not-found');
     $('.result').hide().removeClass('result-active');
 
-    // show bootstrap spinner
+    // show bootstrap spinner while user is typing
     if ($('#cep-in').val().length !== 0) {
         $(() => {
             $('.spinner-border').show(300);
@@ -40,11 +45,11 @@ $('#cep-in').on('input', () => {
         success: cepData => {
             if (!cepData.erro) {
                 console.log(cepData);
-                // arranging the area for the data
+                // arranging the result area for the CEP data
                 $('.catch-data').addClass('catch-data-active');
                 $('.user-input').addClass('user-input-active');
 
-                // showing cep data
+                // inserting cep data on html
                 for (let data in cepData) {
                     if (cepData[data]) {
                         const cepDataParagraph = document.createElement('p');
@@ -53,24 +58,30 @@ $('#cep-in').on('input', () => {
                     }
                 }
 
+                // inserting map iframe based on CEP data on html
                 const mapData = document.createElement('iframe');
                 $(mapData).attr({ id: 'map-iframe', src: `https://maps.google.com/maps?q=${cepData.siafi} ${cepData.localidade}, ${cepData.uf}&t=k&z=15&ie=UTF8&iwloc=&output=embed`, frameBorder: '0', scrolling: 'no', marginHeight: '0', marginWidth: '0' });
                 $('.map').append(mapData);
 
-                // show results as user type
+                // getting cep data amount
+                const cepDataAmount = Object.values(cepData).filter(data => {
+                    return data !== '';
+                }, []);
+
+                // showing results
+                if (cepDataAmount.length > 7) $('.viaCEP-api-data').css('font-size', '1.7vw');
+                else $('.viaCEP-api-data').css('font-size', '2vw'); // change font-size based on cep data amount
+
                 $('.spinner-border').delay(600).hide(0);
                 $('.result').show().addClass('result-active');
             } else {
-                // show results as user type
+                // showing results
                 $('.spinner-border').delay(300).hide(0);
                 $('.user-input').addClass('not-found'); // error message - api error return (CEP don't exists)
             }
         },
-        error: () => {
-            if ($('#cep-in').val().length > 8) {
-                $('.spinner-border').hide();
-                $('.user-input').addClass('invalid'); // error message - api request error (invalid CEP request)
-            }
+        error: errorMessage => {
+            console.error(errorMessage);
         }
     });
 });
